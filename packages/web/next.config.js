e989a33955e1de655b521160b187af72cdf0d720
Brian Olingay <brian@crowdmetric.com>
2019-06-07 19:59:44 +0800

    set typescript & apollo, doing antd.

---
+++ packages/web/next.config.js
@@ -0,0 +1,36 @@
+const withTypescript = require("@zeit/next-typescript");
+const withCss = require("@zeit/next-css");
+
+const wCss = withCss({
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === "function" ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: "null-loader",
+      });
+    }
+    config.node = {
+      ...(config.node || {}),
+      net: "empty",
+      tls: "empty",
+      dns: "empty",
+    };
+    return config;
+  },
+});
+
+module.exports = withTypescript({ ...wCss });
