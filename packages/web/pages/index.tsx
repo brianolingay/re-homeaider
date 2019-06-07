2a68ef19dda7b13940fcbd7c741de2c8291953e7
Brian Olingay <brian@crowdmetric.com>
2019-06-07 11:31:03 +0800

    setup for with apollo.

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,17 +1,3 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
 
-export default () => (
-  <ul>
-    <li>
-      <Link href="/a" as="/a">
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/b" as="/b">
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
-)
+export default () => <div>Hello World</div>;
