c16b904579aee00d875eeaf84391149dd6c5cfb7
Brian Olingay <brian@crowdmetric.com>
2019-06-25 10:37:26 +0800

    added statement field validation.

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,5 +2,6 @@ export * from "./yupSchemas/category";
 export * from "./yupSchemas/constants";
 export * from "./yupSchemas/role";
 export * from "./yupSchemas/service";
+export * from "./yupSchemas/serviceAction";
 export * from "./yupSchemas/subscription";
 export * from "./yupSchemas/user";
